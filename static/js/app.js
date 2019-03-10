a4c201a3d5a74073a701044a3b90c2237ce459e5
Omar <Oabusheikh@utexas.edu>
2019-03-10 17:59:51 -0500

    added alerts to account for all inputs; level 1 of assignment now complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,55 @@
-// ### Level 1: Automatic Table and Date Search
+/**   Level 1: Automatic Table and Date Search 
+ *        Using the UFO dataset provided (array of JavaScript objects), 
+ *                - Populate an html table w/ its values, using columns `date/time`, `city`, `state`, `country`, `shape`, & `comment`.
+ *        Use date input form in HTML & use JavaScript to listen for input & filter `datetime` column for matching results.
+*/
 
-// * Create a basic HTML web page
-// * Using the UFO dataset provided (array of JavaScript objects), 
-//         - write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-//         - Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
-// * Use a date form in your HTML document and write JavaScript code that will listen for events and search 
-//   through the `date/time` column to find rows that match user input.
-
-/**
- * Helper function to select UFO data
- * Returns an array of values
- * @param {array} rows
- * @param {integer} index
- * index 0 - datetime
- * index 1 - city
- * index 2 - state
- * index 3 - country
- * index 4 - shape
- * index 5 - durationMinutes
- * index 6 - comments
+/**  UFO dataset Metadata - Array of Objects:
+ *      0:datetime, 1:city, 2:state, 3:country, 4:shape, 5:durationMinutes, 6:comments
  */
 
-function unpack(rows, index) {
-	return rows.map(function (row) {
-		return row[index];
-	});}
+var tableData = data;                           // from data.js
 
-// from data
-var tableData = data;
+function unpackArray(arrayRows, indexName){     // parameter = array of objects & desired filter
+        
+        return arrayRows.map(row=>row[indexName]);
+                                          }     // return ⟶ array of filtered values
 
-function handleSubmit() {               // date entered by user
+function handleSubmit() {                       // invoked when date entered by user
 	d3.event.preventDefault(); // Prevent the page from refreshing
-        var date_input = d3.select("#datetime").node().value; // Select the input value from the form
-        //console.log("Hello: ", date_input); // for checking; 
+        var userDateEntry = d3.select("#datetime").node().value; // Select the input value from the form
+        //console.log("Hello: ", userDateEntry); // for checking; 
         //d3.select("#filterdate-btn").node().value = ""; // clear the input value
-        date_data = tableData.filter((d,i)=>d.datetime===date_input)
- 	buildTable(date_data); // Build the plot with the new date selected; 
-                        }               // dataset filtered by date & passed ⟶ buildTable(filtered_dates)
+        validDate(userDateEntry)        // return ⟶ validDate(userDateEntry) ⟶ buildTable(filteredDatesArray) ⟶ Build the plot with the new date selected;;
+                        };                      // dataset filtered by date & passed to ⟶ validDate(userDateEntry); which will check & pass to ⟶ buildTable(filtered_dates)
 
-function buildTable(input_data) {       // columns needed: datetime, city, state, country, shape, durationMinutes, comments; 
- 	var tbody = d3.select("#ufo-table").select("tbody");
-        var datetime = unpack(input_data, 'datetime');
-        var city = unpack(input_data, 'city');
-        var state = unpack(input_data, 'state');
-        var country = unpack(input_data, 'country');
-        var shape = unpack(input_data, 'shape');
-        var durationMinutes = unpack(input_data, 'durationMinutes');
-        var comments = unpack(input_data, 'comments');
-        d3.select("#ufo-table").select("tbody").selectAll('tr').remove() //clear table
-	for (var i = 0; i < input_data.length; i++) {
+function validDate(dateInput) {                 // parameter = date input by user
+        if (availableDates.includes(dateInput)) {
+                filteredDatesArray = tableData.filter((datum,index)=>datum.datetime===dateInput) // returns array of objects w/ dates filtered
+                return success(filteredDatesArray), buildTable(filteredDatesArray);
+                                                } // Build the plot with the new date selected;;
+        else {return warning(), buildTable(tableData)}
+                              };                // checks validity & return ⟶ warning or buildTable(filteredDatesArray)
+function warning(){                             // ALERT: invalid date
+        var str = 'Date entered is unavailable (or invalid)';
+        alert(`${str},  This dataset only contains ${availableDates.length} sightings on the following unique dates:\n\n${availableDates.join(",\t")}\n\nPlease try again.`)        
+                  };
+function success(inputArray){                   // ALERT: success
+                alert(`${inputArray.length} sightings found!`)        
+                            };
+function buildTable(inputArray) {               // parameter = array for which to populate table;
+        // columns needed: datetime, city, state, country, shape, durationMinutes, comments; 
+        var durationMinutes = unpackArray(inputArray, 'durationMinutes');
+        var comments = unpackArray(inputArray, 'comments');
+        var datetime = unpackArray(inputArray, 'datetime');
+        var country = unpackArray(inputArray, 'country');
+        var shape = unpackArray(inputArray, 'shape');
+        var state = unpackArray(inputArray, 'state');
+        var city = unpackArray(inputArray, 'city');
+
+        var tbody = d3.select("#ufo-table").select("tbody");
+        tbody.selectAll('tr').remove() //clear table
+	for (var i = 0; i < inputArray.length; i++) {
 		trow = tbody.append("tr");
 		trow.append("td").text(datetime[i]);
 		trow.append("td").text(city[i]);
@@ -58,48 +59,31 @@ function buildTable(input_data) {       // columns needed: datetime, city, state
                 trow.append("td").text(durationMinutes[i]);
                 trow.append("td").text(comments[i]);
                 }
-                                };      // clear existing table & builds new one w/ passed data
+                                };              // clear existing table & return ⟶ new table in HTML
 
-buildTable(tableData);
+buildTable(tableData);                          // initialize table when first loading page
+var availableDates = uniqueDates();             // global array variable containing unique dates
 
 d3.select("#date-btn")
-        .on("click", handleSubmit);     // Add event listener for submit button
+        .on("click", handleSubmit);             // Add event listener for submit button
 
-// ### Level 2: Multiple Search Categories (Optional)
-// * Complete all of Level 1 criteria.
-// * Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters &
-//   search for UFO sightings using the following criteria based on the table columns:
-//   1. `date/time`
-//   2. `city`
-//   3. `state`
-//   4. `country`
-//   5. `shape`
+function uniqueDates()  {                       // Determines # of unique dates –––
+var flags = [], availableDates = [], l = data.length, i;
+for( i=0; i<l; i++) 
+        {
+        if( flags[data[i].datetime]) continue;
+        flags[data[i].datetime] = true;
+        availableDates.push(data[i].datetime);
+        }
+        return availableDates;
+                        };                      // needed to validate user entry
+// –––––––––––––––––––––––––––––– ### Level 2: Multiple Search Categories (Optional)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//d3.selectAll("tr").filter(function(datum, index) { return index & 1; });
-// filter returns a new selection of elements that evaluated to true; can be returned blank
-// filter by d3 selector 
-// filter by function ⟶ evaluated for entire selection in order: ⟶ datum(d) ⟶ index(i) ⟶ selected_group_of_nodes
-// this == current nodes[i]
-
-// d3.selectAll("tr").filter((datum, index)=>datum.datetime='1/1/2010')
-
-
-// d3.select('#ufo-table').select('tbody').selectAll('tr').select('td')
+/* Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters &
+ * search for UFO sightings using the following criteria based on the table columns:
+ * 1. `date/time`
+ * 2. `city`
+ * 3. `state`
+ * 4. `country`
+ * 5. `shape`
+*/
